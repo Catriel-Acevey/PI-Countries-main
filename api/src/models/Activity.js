95267a39c04d4d9438fff259273a08ccf358bb82
Catriel <aceveycatriel@gmail.com>
2021-11-21 03:42:50 -0300

    Modificacion a modelos y creo las conexiones

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
-  sequelize.define("tourist_activity", {
+  sequelize.define("activity", {
     name: {
       type: DataTypes.STRING,
     },
